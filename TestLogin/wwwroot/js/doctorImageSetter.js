7c0f7f09adf1dfe09b48125061ddc4b0c1f3e48a
BohdanMolchanov <bodia007killer@gmail.com>
2019-05-22 01:01:24 +0300

    Added filter search by name on ChooseDoctor page (without normal styles)

--- TestLogin/wwwroot/js/doctorImageSetter.js
+++ TestLogin/wwwroot/js/doctorImageSetter.js
@@ -1,9 +1,14 @@
 let doctorContainer = document.getElementById("doctor-container");
+let doctorSelect = document.getElementById("doctor-name-select");
+let doctorChanger = document.getElementById("doctor-name-change");
 
-console.log(doctorContainer);
+doctorChanger.addEventListener("input", filter);
+
+// console.log(doctorContainer);
 loadImages();
+fillDoctorSelect();
 function loadImages() {
-  console.log(doctorContainer.children);
+  // console.log(doctorContainer.children);
   for (let i = 0; i < doctorContainer.children.length; i++) {
     // console.log(doctorContainer.children[i]);
     let form = doctorContainer.children[i].children[0];
@@ -12,7 +17,34 @@ function loadImages() {
       "src",
       "../images/doctors/" + employeeId + ".jpg"
     );
-    console.log(form.children[1]);
+    // console.log(form.children[1]);
   }
 }
-function fillDoctorSelect() {}
+function fillDoctorSelect() {
+  for (let i = 0; i < doctorContainer.children.length; i++) {
+    let doctorName =
+      doctorContainer.children[i].children[0].children[2].firstElementChild
+        .innerText;
+    console.log(doctorName);
+    let newOption = document.createElement("option");
+    // newOption.innerText = doctorName;
+    newOption.setAttribute("value", doctorName);
+
+    doctorSelect.appendChild(newOption);
+  }
+}
+
+function filter() {
+  for (let i = 0; i < doctorContainer.children.length; i++) {
+    if (doctorChanger.value === "") {
+      doctorContainer.children[i].style.display = "flex";
+    } else {
+      let doctorName =
+        doctorContainer.children[i].children[0].children[2].firstElementChild
+          .innerText;
+      if (doctorName.includes(doctorChanger.value)) {
+        doctorContainer.children[i].style.display = "flex";
+      } else doctorContainer.children[i].style.display = "none";
+    }
+  }
+}
