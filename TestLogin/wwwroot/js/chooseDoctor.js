b9fb513c269c92ab9228779321cbd8d778fe8109
BohdanMolchanov <bodia007killer@gmail.com>
2019-05-22 14:06:59 +0300

    Functionality of search is now in chooseDoctor file

--- TestLogin/wwwroot/js/chooseDoctor.js
+++ TestLogin/wwwroot/js/chooseDoctor.js
@@ -1,8 +1,44 @@
-﻿document.querySelector("#doctor-container").addEventListener("submit", e => {
-    e.preventDefault();
-    e.target.firstElementChild.setAttribute("name", "EmployeeId");
-    e.target.submit();
+﻿//const doctorContainer = document.getElementById("doctor-container");
+const doctorSelect = document.getElementById("doctor-name-select");
+let doctorChanger = document.getElementById("doctor-name-change");
+
+doctorChanger.addEventListener("input", filter);
+
+fillDoctorSelect();
+document.querySelector("#doctor-container").addEventListener("submit", e => {
+  e.preventDefault();
+  e.target.firstElementChild.setAttribute("name", "EmployeeId");
+  e.target.submit();
 });
+
+function fillDoctorSelect() {
+  for (let i = 0; i < doctorContainer.children.length; i++) {
+    let doctorName =
+      doctorContainer.children[i].children[0].children[2].firstElementChild
+        .innerText;
+    console.log(doctorName);
+    let newOption = document.createElement("option");
+    // newOption.innerText = doctorName;
+    newOption.setAttribute("value", doctorName);
+
+    doctorSelect.appendChild(newOption);
+  }
+}
+
+function filter() {
+  for (let i = 0; i < doctorContainer.children.length; i++) {
+    if (doctorChanger.value === "") {
+      doctorContainer.children[i].style.display = "flex";
+    } else {
+      let doctorName = doctorContainer.children[
+        i
+      ].children[0].children[2].firstElementChild.innerText.toLowerCase();
+      if (doctorName.includes(doctorChanger.value.toLowerCase())) {
+        doctorContainer.children[i].style.display = "flex";
+      } else doctorContainer.children[i].style.display = "none";
+    }
+  }
+}
 //e.preventDefault();
 //const data = {
 //    EmployeeId: e.target.firstElementChild.value
--- TestLogin/wwwroot/js/doctorImageSetter.js
+++ TestLogin/wwwroot/js/doctorImageSetter.js
@@ -1,50 +1,16 @@
-let doctorContainer = document.getElementById("doctor-container");
-let doctorSelect = document.getElementById("doctor-name-select");
-let doctorChanger = document.getElementById("doctor-name-change");
-
-doctorChanger.addEventListener("input", filter);
-
+const doctorContainer = document.getElementById("doctor-container");
 // console.log(doctorContainer);
 loadImages();
-fillDoctorSelect();
 function loadImages() {
   // console.log(doctorContainer.children);
   for (let i = 0; i < doctorContainer.children.length; i++) {
     // console.log(doctorContainer.children[i]);
-    let form = doctorContainer.children[i].children[0];
+    const form = doctorContainer.children[i].children[0];
     let employeeId = form.children[0].getAttribute("value");
     form.children[1].setAttribute(
       "src",
       "../images/doctors/" + employeeId + ".jpg"
     );
     // console.log(form.children[1]);
-  }
-}
-function fillDoctorSelect() {
-  for (let i = 0; i < doctorContainer.children.length; i++) {
-    let doctorName =
-      doctorContainer.children[i].children[0].children[2].firstElementChild
-        .innerText;
-    console.log(doctorName);
-    let newOption = document.createElement("option");
-    // newOption.innerText = doctorName;
-    newOption.setAttribute("value", doctorName);
-
-    doctorSelect.appendChild(newOption);
-  }
-}
-
-function filter() {
-  for (let i = 0; i < doctorContainer.children.length; i++) {
-    if (doctorChanger.value === "") {
-      doctorContainer.children[i].style.display = "flex";
-    } else {
-      let doctorName = doctorContainer.children[
-        i
-      ].children[0].children[2].firstElementChild.innerText.toLowerCase();
-      if (doctorName.includes(doctorChanger.value.toLowerCase())) {
-        doctorContainer.children[i].style.display = "flex";
-      } else doctorContainer.children[i].style.display = "none";
-    }
   }
 }
